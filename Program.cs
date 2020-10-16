c837de4b9c14a130d044ec133f8b9775780672e8
sbcode42 <ballersabooth@yahoo.com>
2020-10-16 16:16:07 -0500

    finally pushing from save

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KanyeAndRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+        }
+    }
+}
